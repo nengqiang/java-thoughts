9b40f2847500aee87fef2a3f94cc547f02de7d18
nengqiang <henq163@163.com>
2021-06-28 14:01:45 +0800

    longestCommonPrefix2

--- algorithm-demo/src/main/java/com/hnq/study/everyday/LongestCommonPrefix.java
+++ algorithm-demo/src/main/java/com/hnq/study/everyday/LongestCommonPrefix.java
@@ -69,6 +69,20 @@ public class LongestCommonPrefix {
         return res.toString();
     }
 
+    private static String longestCommonPrefix2(String[] strs) {
+        // 0ms
+        if (strs == null || strs.length <= 0) {
+            return "";
+        }
+        String str = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(str) != 0) {
+                str = str.substring(0, str.length() - 1);
+            }
+        }
+        return str;
+    }
+
 }
 
 
