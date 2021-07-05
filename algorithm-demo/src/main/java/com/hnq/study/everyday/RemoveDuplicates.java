c6cac4d63baac7889004bdef098830c8227b6a87
nengqiang <henq163@163.com>
2021-07-05 19:58:51 +0800

    removeElement

--- algorithm-demo/src/main/java/com/hnq/study/everyday/RemoveDuplicates.java
+++ algorithm-demo/src/main/java/com/hnq/study/everyday/RemoveDuplicates.java
@@ -88,7 +88,7 @@ public class RemoveDuplicates {
         if (nums.length == 0|| nums.length == 1) {
             return nums.length;
         }
-        int i = 0, j =1;
+        int i = 0, j = 1;
         while (j < nums.length) {
             if (nums[i] != nums[j]) {
                 i++;
