780af786e7195906d5ed88528fe83a23f96344fd
nengqiang <henq163@163.com>
2024-10-18 10:17:20 +0800

    NumRookCaptures

---
+++ algorithm-demo/src/main/java/com/hnq/study/everyday/quarter202403/NumRookCaptures.java
@@ -0,0 +1,53 @@
+package com.hnq.study.everyday.quarter202403;
+
+/**
+ * @author henengqiang
+ * @date 2024/10/18
+ */
+public class NumRookCaptures {
+
+    public static void main(String[] args) {
+        NumRookCaptures n = new NumRookCaptures();
+        char[][] c1 = new char[][]{{'.', '.', '.', '.', '.', '.', '.', '.'}, {'.', '.', '.', 'p', '.', '.', '.', '.'}, {'.', '.', '.', 'R', '.', '.', '.', 'p'}, {'.', '.', '.', '.', '.', '.', '.', '.'}, {'.', '.', '.', '.', '.', '.', '.', '.'}, {'.', '.', '.', 'p', '.', '.', '.', '.'}, {'.', '.', '.', '.', '.', '.', '.', '.'}, {'.', '.', '.', '.', '.', '.', '.', '.'}};
+        System.out.println(n.numRookCaptures(c1));
+
+        char[][] c2 = new char[][]{{'.', '.', '.', '.', '.', '.', '.', '.'}, {'.', 'p', 'p', 'p', 'p', 'p', '.', '.'}, {'.', 'p', 'p', 'B', 'p', 'p', '.', '.'}, {'.', 'p', 'B', 'R', 'B', 'p', '.', '.'}, {'.', 'p', 'p', 'B', 'p', 'p', '.', '.'}, {'.', 'p', 'p', 'p', 'p', 'p', '.', '.'}, {'.', '.', '.', '.', '.', '.', '.', '.'}, {'.', '.', '.', '.', '.', '.', '.', '.'}};
+        System.out.println(n.numRookCaptures(c2));
+
+        char[][] c3 = new char[][]{{'.', '.', '.', '.', '.', '.', '.', '.'}, {'.', '.', '.', 'p', '.', '.', '.', '.'}, {'.', '.', '.', 'p', '.', '.', '.', '.'}, {'p', 'p', '.', 'R', '.', 'p', 'B', '.'}, {'.', '.', '.', '.', '.', '.', '.', '.'}, {'.', '.', '.', 'B', '.', '.', '.', '.'}, {'.', '.', '.', 'p', '.', '.', '.', '.'}, {'.', '.', '.', '.', '.', '.', '.', '.'}};
+        System.out.println(n.numRookCaptures(c3));
+    }
+
+    /**
+     * 方向数组
+     */
+    public int numRookCaptures(char[][] board) {
+        // 0ms
+        int cnt = 0, st = 0, ed = 0;
+        int[] dx = {0, 1, 0, -1}, dy = {1, 0, -1, 0};
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j] == 'R') {
+                    st = i;
+                    ed = j;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int step = 0;;++step) {
+                int tx = st + step * dx[i];
+                int ty = ed + step * dy[i];
+                if (tx < 0 || tx >= 8 || ty < 0 || ty >= 8 || board[tx][ty] == 'B') {
+                    break;
+                }
+                if (board[tx][ty] == 'p') {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+        return cnt;
+    }
+
+}
